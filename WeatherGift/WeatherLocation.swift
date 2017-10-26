8949e43bc860806365d9a38c9806c2bd66a68dca
Vincent Stephen Huang <hvincentstephen@Vincents-MacBook-Pro.local>
2017-10-26 19:05:39 -0400

    Completed through 7.21

--- WeatherGift/WeatherLocation.swift
+++ WeatherGift/WeatherLocation.swift
@@ -11,10 +11,25 @@ import Alamofire
 import SwiftyJSON
 
 class WeatherLocation {
+    
+    struct DailyForecast {
+        var dailyMaxTemp: Double
+        var dailyMinTemp: Double
+        var dailyDate: Double
+        var dailySummary: String
+        var dailyIcon: String
+    }
+    
+    
     var name = ""
     var coordinates = ""
     var currentTemp = "--"
     var currentSummary = ""
+    var currentIcon = ""
+    var currentTime = 0.0
+    var timeZone = ""
+    
+    var dailyForecastArray = [DailyForecast]()
     
     func getWeather(completed: @escaping() -> ()) {
         let weatherURL = urlBase + urlAPIKey + coordinates
@@ -23,6 +38,7 @@ class WeatherLocation {
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
+                
                 if let temperature = json["currently"]["temperature"].double {
                     let roundedTemp = String(format: "%3.f", temperature)
                     self.currentTemp = roundedTemp + "°"
@@ -30,14 +46,47 @@ class WeatherLocation {
                     print("Could not return a temperature")
                 }
                 
-                
-                
                 if let weatherSummary = json["daily"]["summary"].string {
                     self.currentSummary = weatherSummary
                 } else {
                     print("Could not return a summary")
                 }
-
+                
+                if let icon = json["currently"]["icon"].string {
+                    self.currentIcon = icon
+                } else {
+                    print("Could not return an icon")
+                }
+                
+                if let timeZone = json["timezone"].string {
+                    self.timeZone = timeZone
+                } else {
+                    print("Could not return a time zone")
+                }
+                
+                if let time = json["currently"]["time"].double {
+                    self.currentTime = time
+                } else {
+                    print("Could not return a time")
+                }
+                
+                let dailyDataArray = json["daily"]["data"]
+                self.dailyForecastArray = []
+                for day in 1...dailyDataArray.count - 1 {
+                    let maxTemp = json["daily"]["data"][day]["temperatureHigh"].doubleValue
+                    let minTemp = json["daily"]["data"][day]["temperatureLow"].doubleValue
+                    let dateValue = json["daily"]["data"][day]["time"].doubleValue
+                    let icon = json["daily"]["data"][day]["icon"].stringValue
+                    let dailySummary = json["daily"]["data"][day]["summary"].stringValue
+                    let newDailyForecast = DailyForecast(dailyMaxTemp: maxTemp,
+                                                         dailyMinTemp: minTemp,
+                                                         dailyDate: dateValue,
+                                                         dailySummary: dailySummary,
+                                                         dailyIcon: icon)
+                
+                    self.dailyForecastArray.append(newDailyForecast)
+                    
+                }
             case .failure(let error):
                 print(error)
             }
